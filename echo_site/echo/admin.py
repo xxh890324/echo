76272148507c9b7578485019afd2631d7d4578b0
xxh890324 <402719462@qq.com>
2017-02-16 10:31:50 +0000

    第三天提交

--- echo_site/echo/admin.py
+++ echo_site/echo/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 
 #注册模块
-from .models import Node
+from .models import Node,Server,Device
 
 class NodeAdmin(admin.ModelAdmin):
     list_display = ('node_name','node_address','node_signer')
@@ -13,3 +13,20 @@ class NodeAdmin(admin.ModelAdmin):
         obj.save()
 
 admin.site.register(Node,NodeAdmin)
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ('server_ip','server_sertype','server_local','server_signer','server_remarks')
+    exclude = ('server_signer',)
+    def save_model(self,request,obj,form,change):
+        obj.server_signer = str(request.user)
+        obj.save()
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_name','device_ip','device_devtype','device_vendor','device_signer','device_remarks')
+    exclude = ('device_signer',)
+    def save_model(self,request,obj,form,change):
+        obj.device_signer = str(request.user)
+        obj.save()
+
+admin.site.register(Server,ServerAdmin)
+admin.site.register(Device,DeviceAdmin)
--- echo_site/echo/models.py
+++ echo_site/echo/models.py
@@ -21,3 +21,50 @@ class Node(models.Model):
 
 	def __unicode__(self):
 		return self.node_name
+
+class Server(models.Model):
+
+	node = models.ForeignKey(Node,on_delete=models.PROTECT)
+
+	#服务器用途分类
+	sertype = (
+		(u'业务服务器','业务服务器'),
+		(u'平台服务器','平台服务器'),
+		(u'画面合成服务器','画面合成服务器'),
+		(u'数据库服务器','数据库服务器'),
+		(u'中间件服务器','中间件服务器'),
+		(u'其他','其他')
+	)
+
+	server_code = models.CharField(verbose_name='服务器编号',max_length=100)
+	server_local = models.CharField(verbose_name='所在节点',max_length=100)
+	server_sertype = models.CharField(verbose_name='服务器类型',max_length=100,choices=sertype)
+	server_ip = models.CharField(verbose_name='服务器IP地址',max_length=100)
+	server_signer = models.CharField(verbose_name='登记人',max_length=100,default='system')
+	server_signtime = models.DateField(auto_now_add=True)
+	server_remarks = models.CharField(verbose_name='备注',max_length=255,blank=True)
+
+	def __unicode__(self):
+		return self.server_code
+
+class Device(models.Model):
+
+	node = models.ForeignKey(Node,on_delete=models.PROTECT)
+
+	devtype = (
+		(u'摄像机','摄像机'),
+		(u'编码器','编码器'),
+		(u'解码器','解码器'),
+		(u'其他','其他'),
+	)
+
+	device_name = models.CharField(verbose_name='设备名称',max_length=255)
+	device_ip = models.CharField(verbose_name='设备IP',max_length=100)
+	device_devtype = models.CharField(verbose_name='设备类型',max_length=200,choices=devtype)
+	device_vendor = models.CharField(verbose_name='设备厂商',max_length=100)
+	device_signer = models.CharField(verbose_name='登记人',max_length=100,default='system')
+	device_signtime = models.DateField(auto_now_add=True)
+	device_remarks = models.CharField(verbose_name='备注',max_length=255,blank=True)
+
+	def __unicode__(self):
+		return self.device_name
